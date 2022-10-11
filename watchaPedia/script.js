f5d169b6e1e66bc75fe443193bd82905522ead08
wooooooongs <pires11233@gmail.com>
2022-10-12 02:03:22 +0900

    행별로 슬라이드 효과 넣기

--- watchaPedia/script.js
+++ watchaPedia/script.js
@@ -8,7 +8,6 @@
     // [[1,2,3,4,5],[1,2,3]] -> flat() -> [1,2,3,4,5,1,2,3]
     posterArr.push(...Array(itemsUl[i].children.length).fill(1).map((v, i) => v + i));
     posterArr.flat();
-    console.log(posterArr);
   })
   
   posterNum.forEach((v, i) => {
@@ -16,37 +15,55 @@
   });
 })()
 
-
+/*
 // 포스터 슬라이드 효과
-// 고민 1. 전역변수 말고 방법이 없을까? -> 모듈화?
-let arrow_count = 0;
+// [[1,2,3],[1,2],[1,2,3,4,5]]
+// 1을 찾고 그 다음 1을 찾아서 그 사이에만 주기?
+*/
+let arrow_count = [0, 0, 0];
 
-function moveItem(n) {
-  const item = document.querySelectorAll('.item');
+function a(e) {
+  const itemsUl = document.querySelectorAll(".itemsUl");
   
-  // translate(509.65%) 중 숫자만 num에 저장
-  let num = Number(item[0].style.transform.slice(10).slice(0, -2));
-  
-  if (n.target.innerText === 'next') {
-    if (arrow_count < item.length / 5 - 1) {
-      num -= 509.65;
-      arrow_count++;
-      console.log('next', num, 'count', arrow_count);
+  itemsUl.forEach((ul, i) => {
+      // itemsUl > li의 배열
+      if (itemsUl[i] === e.target.parentElement.parentElement.children[0]){
+      console.log('yes');
+        
+      const itemsUl_children = itemsUl[i].children;
+    
+      // translate(509.65%) 중 숫자만 저장
+      let translateValue = Number(
+        itemsUl_children[0].style.transform.slice(10).slice(0, -2)
+      );
+      
+      // next버튼 클릭 시
+      if (e.target.innerText === "next") {
+        if (arrow_count[i] < itemsUl_children.length / 5 - 1) {
+          translateValue -= 509.65;
+          arrow_count[i]++;
+          console.log("next", translateValue, "count", arrow_count[i]);
+        }
+      } else if (arrow_count[i] !== 0) {
+        console.log("prev", translateValue, "count", arrow_count[i]);
+        translateValue = parseFloat(translateValue);
+        translateValue += 509.65;
+        arrow_count[i]--;
+      }
+    
+      // 각 item마다 translate 적용하기
+      for (v of itemsUl_children) {
+        v.style.transform = `translate(${translateValue}%)`;
+        console.log(arrow_count);
+      };
     }
-  } else if (num !== 0) {
-    num = parseFloat(num);
-    num += 509.65;
-    arrow_count--;
-    console.log('next', num, 'count', arrow_count);
-  }
-  
-  // 각 item마다 translate 적용하기
-  item.forEach((v, i) => {
-    v.style.transform = `translate(${num}%)`;
-  });
+  })
 }
-const arrow_prev = document.querySelector('.arrow_prev');
-const arrow_next = document.querySelector('.arrow_next');
 
-arrow_prev.addEventListener('click', moveItem);
-arrow_next.addEventListener('click', moveItem);
+const arrow_prev = document.querySelectorAll('.arrow_prev');
+const arrow_next = document.querySelectorAll('.arrow_next');
+
+arrow_prev.forEach((v, i) => {
+  arrow_prev[i].addEventListener('click', a);
+  arrow_next[i].addEventListener('click', a);
+})
